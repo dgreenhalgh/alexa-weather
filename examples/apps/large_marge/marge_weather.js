b7a83d2ad8392cb4b6d1e0232f0538e328145f26
David Greenhalgh <davidrgreenhalgh@gmail.com>
2016-03-31 10:48:53 -0400

    POSTing to new endpoint

--- examples/apps/large_marge/marge_weather.js
+++ examples/apps/large_marge/marge_weather.js
@@ -1,19 +1,17 @@
 'use strict';
 var _ = require('lodash');
 var requestPromise = require('request-promise');
-var ENDPOINT = 'http://services.faa.gov/airport/status/'; // TODO: Replace placeholder with actual endpoint
+var ENDPOINT = 'http://large-marge-server.herokuapp.com/start';
 
 function MargeWeather() {
 
 }
 
-// TODO: Change method to POST
-// TODO: Remove Charlotte airport code from URI
 // TODO: Handle timeframe
 MargeWeather.prototype.postWeatherRequest = function(timeFrame) {
   var options = {
-    method: 'GET',
-    uri: ENDPOINT + 'CLT',
+    method: 'POST',
+    uri: ENDPOINT,
     json: true
   };
 
